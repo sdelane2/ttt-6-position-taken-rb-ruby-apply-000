05cc2d67d913982107f9fceaf8fcfafc323433cd
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-14 22:01:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,5 @@
 def position_taken?(board, index)
-  index = input.to_i - 1
-  if board["#{index}"] == (" ") || ("") || nil 
+  if [board("#{index}")] == (" ") || ("") || nil 
     return FALSE
   else
     return TRUE
